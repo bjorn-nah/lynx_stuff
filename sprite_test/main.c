5bb6b73e200b808731babca71a5dc9278edda787
Bjorn Nah <bjorn.nah@gmail.com>
2021-11-25 16:53:26 +0100

    Add scroll and scale variations

--- sprite_test/main.c
+++ sprite_test/main.c
@@ -20,12 +20,14 @@ SCB_REHV_PAL  skullsprite =  {
   REHV,
   0x01,
   0x0000,
-  &skull,
+  skull,
   20, 50,
-  0x0500, 0x0500,
+  0x0100, 0x0100,
   // palette values can be found in .pal file provided by sprpck
   {0x01,0x23,0x45,0x67,0x89,0xAB,0xCD,0xEF}
   };
+
+int dirrection, grow;
 
 void initialize()
 {
@@ -41,6 +43,9 @@ void initialize()
 	tgi_setbgcolor(COLOR_BLACK);
 
 	tgi_clear();
+	
+	dirrection = 1;
+	grow = 1;
 }
 
 void show_screen()
@@ -51,6 +56,31 @@ void show_screen()
 	tgi_setcolor(COLOR_WHITE);
 	tgi_outtextxy(0, 0, "Sprite basics");
 	tgi_sprite(&skullsprite);
+	
+	if(dirrection){
+		skullsprite.hpos++;
+	}else{
+		skullsprite.hpos--;
+	}
+	if(skullsprite.hpos==0){
+		dirrection=1;
+	}
+	if(skullsprite.hpos==120){
+		dirrection=0;
+	}
+	if(grow){
+		skullsprite.hsize+=0x0010;
+		skullsprite.vsize+=0x0010;
+	}else{
+		skullsprite.hsize-=0x0010;
+		skullsprite.vsize-=0x0010;
+	}
+	if(skullsprite.hsize==0x0010){
+		grow=1;
+	}
+	if(skullsprite.hsize==0x0500){
+		grow=0;
+	}	
 	
 	tgi_updatedisplay();
 }
